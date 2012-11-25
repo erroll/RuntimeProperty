492bd230cebaf8fe0e1cc258f8ee116ac664ec0d
Erroll Kappes <kapp.es@icloud.com>
2012-11-25 12:15:23 +0100

    added docu to category implementation

--- RuntimeProperty/NSObject+addRuntimeProperty.m
+++ RuntimeProperty/NSObject+addRuntimeProperty.m
@@ -11,25 +11,42 @@
 @implementation NSObject (addRuntimeProperty)
 
 - (void)setProperty:(id)property forKey:(char const * const)key withPolicy:(AssociantionPolicy)policy {
+    
+    // validate input
     NSAssert(property,@"PROPERTY must not be nil.");
     NSAssert(key,@"KEY must not be nil.");
     NSAssert(policy!=0, @"POLICY must not be null.");
     NSAssert([self isValidPolicy:policy],@"POLICY must be valid.");
+    
+    // associate object
     objc_setAssociatedObject(self, key, property, policy);
 }
 
 - (id)getPropertyForKey:(char const * const)key
 {
+    // validate input
     NSAssert(key,@"KEY must not be nil.");
     NSAssert(objc_getAssociatedObject(self, key)!=nil,@"there is no object for the given KEY.");
+    
+    // return object
     return objc_getAssociatedObject(self, key);
 }
 
 - (BOOL)hasPropertyForKey:(char const * const)key
 {
+    // validate input
     NSAssert(key,@"KEY must not be nil.");
-    return objc_getAssociatedObject(self, key)?YES:NO;
+    
+    // return logical result
+    return objc_getAssociatedObject(self, key) ? YES : NO;
 }
+
+/**
+ checks if a given policy is valid
+ 
+ @param policy - the memory management policy
+ @return logical result indicating if policy is valid
+ */
 
 - (BOOL)isValidPolicy:(AssociantionPolicy)policy
 {
