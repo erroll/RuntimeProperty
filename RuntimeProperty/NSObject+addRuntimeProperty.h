52a648d13c27e708dc01851acd06dd0f197570ac
Erroll Kappes <kapp.es@icloud.com>
2012-11-25 12:10:28 +0100

    added docu to category header file

--- RuntimeProperty/NSObject+addRuntimeProperty.h
+++ RuntimeProperty/NSObject+addRuntimeProperty.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
+/**
+ The AssociantionPolicy enum determines the association memory management type.
+ */
+
 typedef enum {
     ASSIGN_POLICY = OBJC_ASSOCIATION_ASSIGN,
     RETAIN_NONATOMIC_POLICY = OBJC_ASSOCIATION_RETAIN_NONATOMIC,
@@ -19,10 +23,30 @@ typedef enum {
 
 @interface NSObject (addRuntimeProperty)
 
+/**
+ Sets the association between any property for a specific key.
+ 
+ @param property - the associated object
+ @param key - the key to identify the associated object
+ @param policy - the memory management policy
+ */
 - (void)setProperty:(id)property forKey:(char const * const)key withPolicy:(AssociantionPolicy)policy;
 
+
+/**
+ returns the associated object for a given key.
+ 
+ @param key - the key to identify the associated object
+ @return the assiciated object
+ */
 - (id)getPropertyForKey:(char const * const)key;
 
+/**
+ determines if there is a object for a given key.
+ 
+ @param key - the key to identify the associated object
+ @return a boolean value indicating if there is a associated object for a given key
+ */
 - (BOOL)hasPropertyForKey:(char const * const)key;
 
 @end
